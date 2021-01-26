5d96711ff40ade684c964d3865c5671237550a3a
Kamal Nadjieb <kamal.nadjieb@gmail.com>
2021-01-26 20:07:42 +0700

    Add Mocks (#2)
    * Add Mocks
    
    * fix coverage

--- plugin.go
+++ plugin.go
@@ -4,6 +4,8 @@ import (
 	"context"
 )
 
+//go:generate mockery --name=Plugin --structname=Plugin --filename=plugin.go --output=gawetest --outpkg=gawetest
+
 // Plugin defines the interface that a plugin must have
 type Plugin interface {
 	OnJobStart(ctx context.Context, job IdentifiableJob) context.Context
